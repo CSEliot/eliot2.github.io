236617653b35ce3d0d8b5a87f922379eacf4f46a
Eliot <eliot2@umbc.edu>
2015-07-07 21:44:55 -0400

    Testing Kanye

--- Kanye/source/Start.js
+++ Kanye/source/Start.js
@@ -1,3 +1,12 @@
+//Code by: Eliot Carney-Seim
+// References:	https://en.wikipedia.org/wiki/Conway's_Game_of_Life
+// 				https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide
+//				http://www.w3schools.com/js/
+//				http://d3js.org/
+// Kanye's Game of Life	   
+
+
+
 var w = window.innerWidth,
     h = window.innerHeight,
     columns = 30,
@@ -7,24 +16,23 @@ var w = window.innerWidth,
     radius = Math.min(Math.floor(w/(2*columns)),Math.floor(h/(2*rows)));
 var canvas = new BuildCanvas(rows,columns);
 canvas.reset();
-var svg = d3.select("body").append("svg:svg")
+var vis = d3.select("body").append("svg:svg")
     .attr("width", w)
     .attr("height", h);
-var circle = svg.selectAll("circle");
+var circle = vis.selectAll("circle");
 
-    canvas.step();
+function PlayLoop(){
 	
+    canvas.step();
+	console.log("Looping!");
     circle = circle.data(canvas.aliveCells(),function(d){return d.n});
-    circle.enter().append("circle")
+    circle.enter().append("image")
+			.attr("xlink:href", "kanye.png")
             .attr("cx", function(d){return d.x*wRatio + radius})
             .attr("cy", function(d){return d.y*hRatio + radius})
-            .transition().duration(500)
-                .attr("r", radius)
-                .style("fill","#000000");;
-    circle.exit()
-        .style("fill","#000000")
-        .transition().duration(500)
-            .attr("r", 0)
-        .remove();
-    setTimeout(arguments.callee,3000);
+            
+    //circle.exit().remove();
+    setTimeout(PlayLoop,2000);
+}
 
+PlayLoop();
